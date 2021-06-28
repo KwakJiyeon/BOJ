1e044877ece6e42c7687edbbf1e4968a11e2ee44
KwakJiyeon <75545799+KwakJiyeon@users.noreply.github.com>
2021-06-29 00:53:23 +0900

    Add files via upload

---
+++ 2443.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int N = 0;
+	cin >> N; 
+
+	for (int i = 1; i <= N; i++) { 
+
+		for (int j = 1; j < i; j++) {
+			cout << " ";
+		}
+
+		for (int j = 2*(N-i+1)-1; j > 0; j--) {
+			cout << "*";
+		}
+
+		cout << endl;
+	}
+
+}
